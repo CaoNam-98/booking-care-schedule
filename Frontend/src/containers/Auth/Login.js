289801646091d39395dfe74975dbd6d8d1520c6f
Cao Nam <caonam.uit@gmail.com>
2022-11-19 08:52:51 +0700

    inital BF

--- Frontend/src/containers/Auth/Login.js
+++ Frontend/src/containers/Auth/Login.js
@@ -5,6 +5,7 @@ import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
+import { handleLoginApi } from '../../services/userService';
 
 
 class Login extends Component {
@@ -30,8 +31,8 @@ class Login extends Component {
         })
     }
 
-    handleLogin = () => {
-        alert('Hoidan It');
+    handleLogin = async () => {
+        await handleLoginApi(this.state.username, this.state.password);
     }
 
     handleShowHidePassword = () => {
